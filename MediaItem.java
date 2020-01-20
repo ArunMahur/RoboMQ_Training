7dc67c7221c6856fddacad5ebcc31b8b500022c0
Arun Mahur <arunmahurrock@gmail.com>
2020-01-20 17:31:00 +0530

    Update MediaItem.java

--- MediaItem.java
+++ MediaItem.java
@@ -1,5 +1,5 @@
 
-public class MediaItem extends Item{
+public abstract class MediaItem extends Item{
 
 	public void GettingDetails() {
 		System.out.println("THis is the overridden methodfrom Item");
